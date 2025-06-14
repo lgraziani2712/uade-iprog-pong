6bbbdc8e18ba0179858c7f0f3d14cc5a6c723868
Luciano Graziani <lgraziani2712@gmail.com>
2025-06-14 18:10:27 -0300

    refactor: simplifica gameloop al eliminar timer personalizado

--- src/game_loop.cpp
+++ src/game_loop.cpp
@@ -1,43 +1,41 @@
 #include "game_loop.hpp"
 #include <iostream>
 #include "configs.hpp"
-#include "timer.hpp"
 
 void gameLoop(SDL_Window* window, SDL_Renderer* renderer,
               PongRender* gameRender) {
-  uint64_t tiempoActual = SDL_GetPerformanceCounter();
+  uint64_t tiempoActual = SDL_GetTicks64();
   uint64_t nuevoTiempo = 0;
   // Esta variable permite realizar movimientos y operaciones en nuestro juego
   // tomando el tiempo como referencia y no el frame, así, podemos calcular lo
   // que se mueve algo por segundo en vez de por frame.
   // REGLA: todos los objetos de una escena deberían actualizarse en base al
   // `delta`.
-  double tiempoDelFrame = 0;
-  double tiempo = 0;
-  const double DELTA_TIME = (1.0 / (double)APP_FPS) * 1000.0;
-
-  Timer<APP_FPS> fps_cap_timer;
+  double deltaBetweenFrames = 0;
+  // Serviría si aplicásemos operaciones de animación basadas en tiempo tiempo.
+  double tiempoDeJuego = 0;
+  // in ms
+  const double DELTA_TIME = (1000.0 / (double)APP_FPS);
 
   while (gameRender->Corriendo()) {
     // 1. Consumir los inputs
     gameRender->ActualizarInputs();
 
     // 2. Calcular Delta Time
-    // Basado en https://gafferongames.com/post/fix_your_timestep/
-    nuevoTiempo = SDL_GetPerformanceCounter();
-    tiempoDelFrame = (double)((nuevoTiempo - tiempoActual) * 1000 /
-                              (double)SDL_GetPerformanceFrequency());
+    nuevoTiempo = SDL_GetTicks64();
+    deltaBetweenFrames = (double)((nuevoTiempo - tiempoActual) * 1000);
     tiempoActual = nuevoTiempo;
-
-    // Técnicamente, habría que hacer un while hasta que tiempoDelFrame
+    // Es posible que en ocasiones, esta variable sea mayor al delta de 16ms,
+    // pero no quieremos que el render calcule con un delta mayor a esa ventana.
+    // Técnicamente, habría que hacer un while hasta que deltaBetweenFrames
     // sea menor a cero en caso de que sea mayor a DELTA_TIME. Pero eso
     // dificultaría el debuggeo.
-    double variableDt =
-        tiempoDelFrame < DELTA_TIME ? tiempoDelFrame : DELTA_TIME;
+    double deltaTime =
+        deltaBetweenFrames < DELTA_TIME ? deltaBetweenFrames : DELTA_TIME;
 
     // 3. Actualizar el juego con el delta time en ms.
-    gameRender->Recalcular(tiempo, variableDt);
-    tiempo += variableDt;
+    gameRender->Recalcular(tiempoDeJuego, deltaTime);
+    tiempoDeJuego += deltaTime;
 
     // 4. Render
     gameRender->Dibujar();
@@ -45,6 +43,6 @@ void gameLoop(SDL_Window* window, SDL_Renderer* renderer,
     SDL_RenderPresent(renderer);
 
     // 5. Dormir el juego hasta el siguiente frame
-    fps_cap_timer.sleep();
+    SDL_Delay(deltaTime);
   }
 }
